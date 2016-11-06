333aa429232b9f854ab15b0e3e8afa379990f5f1
Fanner487 <eamontang1@hotmail.com>
2016-11-06 22:38:58 +0000

    Some basic frames added

---
+++ app/src/main/java/com/example/user/indecisive/fragments/SectionsPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.user.indecisive.fragments;
+
+/**
+ * Created by Eamon on 06/11/2016.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.user.indecisive.activities.MainActivity;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+
+        Fragment fragment = null;
+
+        if(position == 0){
+            fragment = new PickerFragment();
+        }
+        else if(position == 1){
+            fragment = new DrawerFragment();
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "PICKER";
+            case 1:
+                return "DRAWER";
+        }
+        return null;
+    }
+}
